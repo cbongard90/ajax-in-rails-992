6b49610138f8541c0a837e0ab456462b44ce2e06
Christian Bongard <christian.bongard90@gmail.com>
2022-10-14 09:41:31 +0400

    AJAX lecture completed

--- app/javascript/controllers/insert_in_list_controller.js
+++ app/javascript/controllers/insert_in_list_controller.js
@@ -3,16 +3,22 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="insert-in-list"
 export default class extends Controller {
   static targets = ["items", "form"]
-
-  connect() {
-    console.log(this.element)
-    console.log(this.itemsTarget)
-    console.log(this.formTarget)
-  }
+  static values = { position: String } // Add this
 
   send(event) {
     event.preventDefault()
 
-    console.log("TODO: send request in AJAX")
+    fetch(this.formTarget.action, {
+      method: "POST",
+      headers: { "Accept": "application/json" },
+      body: new FormData(this.formTarget)
+    })
+      .then(response => response.json())
+      .then((data) => {
+        if (data.inserted_item) {
+          this.itemsTarget.insertAdjacentHTML(this.positionValue, data.inserted_item)
+        }
+        this.formTarget.outerHTML = data.form
+      })
   }
 }
